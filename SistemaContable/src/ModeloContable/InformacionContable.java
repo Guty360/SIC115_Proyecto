89c8a80d971f4eb408c94be0ade83d6f23166f68
Alexandr-ml <68412919+Alexandr-ml@users.noreply.github.com>
2022-10-04 17:51:38 -0600

    Varias cosas
    Añadida clase informacion contable que sera el punto de entrada para cualquier objeto del modelo contable

---
+++ SistemaContable/src/ModeloContable/InformacionContable.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ModeloContable;
+
+/**
+ *
+ * @author pc
+ */
+public class InformacionContable {
+    private final static LibroMayor libroMayor  = new LibroMayor();
+    
+    private InformacionContable(){}
+    
+    
+}
--- SistemaContable/src/Persistencia/LectorDeArchivos.java
+++ SistemaContable/src/Persistencia/LectorDeArchivos.java
@@ -3,11 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Persistencia;
-
+import com.google.gson.Gson;
 /**
  *
  * @author pc
  */
 public class LectorDeArchivos {
+    private String ruta;
+    private Gson lector;
+
+    public LectorDeArchivos(String ruta, Gson lector) {
+        this.ruta = ruta;
+        this.lector = lector;
+    }
     
+ 
 }
